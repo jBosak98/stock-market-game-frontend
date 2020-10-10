82f31939a85ca58e266a298e1976a88aaf73cd56
jBosak <jbosak98@gmail.com>
2020-10-10 12:56:02 +0200

    getTransactionQuery

---
+++ src/lib/getTransactionsQuery.ts
@@ -0,0 +1,36 @@
+const getTransactionsQuery = `
+    query getTransactionsQuery {
+        getTransactions {
+            id
+            company {
+              ticker
+              id
+              name
+            }
+            pricePerShare
+            quantity
+            createdAt
+            type
+          }
+      }
+  
+`;
+
+export type Transactions = {
+  getTransactions: Transaction[];
+};
+
+type Transaction = {
+  id: number;
+  createdAt: string;
+  quantity: number;
+  type: "PURCHASE" | "DISPOSAL";
+  pricePerShare: number;
+  company: {
+    id: number;
+    name: string;
+    ticker: string;
+  };
+};
+
+export default getTransactionsQuery;
