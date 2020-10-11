5cb6e85c2a99c05fe3beb1dd1274bcab88f02945
jbosak <jbosak98@gmail.com>
2020-10-11 07:53:29 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const getToken = () => localStorage.getItem("token");
 
 const App = () => {
   const client = createClient({
-    url: "http://localhost:8080/graphql",
+    url: "http://188.68.236.168:8080/graphql",
     fetchOptions: () => {
       const token = getToken();
       return {
